5ada8e56e5946efa5209ac0917e2b3f3b46f5623
Randy Douglas <randyweb20@gmail.com>
2018-12-11 11:22:07 -0600

    actualizacion

---
+++ functions.py
@@ -0,0 +1,12 @@
+def thing():
+  print('Hello')
+  print('Fun')
+
+thing()
+print('Zip')
+thing()
+
+big = max('Hello world')
+print(big)
+small = min('Hello world')
+print(small)
---
+++ functions5.py
@@ -0,0 +1,11 @@
+def greet(lang):
+    if lang == 'es':
+        return 'Hola'
+    elif lang == 'fr':
+        return 'Bonjour'
+    elif lang == 'en':
+        return 'Hello'
+
+print(greet('en'), 'Glenn')
+print(greet('es'), 'Sally')
+print(greet('fr'), 'Michael')    
